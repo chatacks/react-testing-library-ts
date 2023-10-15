4a6f5fadd5243bfeb1fa067843d61072cd5c3b81
Thyago <chatack.tc@gmail.com>
2023-10-14 21:20:31 -0300

    [FEAT] - Requisito 1 feito

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,1 +1,18 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+
+import App from '../App';
+
+test('Verifica se componente App possui um conjunto fixo de links de navegação', () => {
+  renderWithRouter(<App />);
+
+  const homeLink = screen.getByRole('link', { name: /home/i });
+  const aboutLink = screen.getByRole('link', { name: /about/i });
+  const favoritePokemonLink = screen.getByRole('link', { name: /favorite pokémon/i });
+
+  expect(homeLink).toBeInTheDocument();
+  expect(aboutLink).toBeInTheDocument();
+  expect(favoritePokemonLink).toBeInTheDocument();
+  // expect(aboutLink).toBeInTheDocument();
+  // expect(favoritePokemonLink).toBeInTheDocument();
+});
